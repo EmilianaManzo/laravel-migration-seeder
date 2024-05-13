31930fe070bb3863fcc7668cf206cdca10a4966c
EmilianaManzo <emiliana.ma96@gmail.com>
2024-05-13 15:26:13 +0200

    [add] update

---
+++ database/migrations/2024_05_13_131126_update_trains_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->tinyInteger('seat_prm')->nullable()->after('number_carriage');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('seat_prm');
+        });
+    }
+};
